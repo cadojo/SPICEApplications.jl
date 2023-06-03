fde8cfb54de0f488dd97a69d95cad755605edc29
Joe Carpinelli <joseph.d.carpinelli@loopy.software>
2023-06-03 18:40:05 -0500

    Adding dskbrief

--- src/SPICEApplications.jl
+++ src/SPICEApplications.jl
@@ -299,7 +299,7 @@ function commnt(
     read && push!(args, "-r")
     delete && push!(args, "-d")
     help && push!(args, "-h")
-    
+
     args = join(args, " ")
     cmd = `$(CSPICE_jll.commnt()) $args $kernelfile $commentfile`
     @debug cmd
@@ -312,8 +312,61 @@ end
 
 """
 DSKBRIEF is a command-line utility program that displays a summary of the spatial coverage and additional attributes of one or more binary Digital Shape Kernel (DSK) files.
+
+# Extended Help
+
+!!! warning
+    All descriptions below were manually parsed from the commandline program's help/usage output.
+
+| Argument | Equivalent | Description | 
+| :--- | :--- | :--- |
+| `single` | `-a` | treat all DSK files as a single file |
+| `gaps` | `-gaps` | display coverage gaps (aplies only when `-a` is used) |
+| `extended` | `-ext` | display extended summaries: these include data type, data class, and time bounds | 
+| `timebounds` | `-tg` | require segment time bounds to match when grouping segments | 
+| `full` | `-full` | display a detailed summary for each segment, including data-type-specific parameters |
+| `sigdigs` | `-d <n>` | display `n` significant digits of floating point values | 
+| `version` | `-v` | display the version of the program | 
+| `help` | `-h` | display help text |
+| `usage` | `-u` | display usage text |
 """
-function dskbrief() end
+function dskbrief(
+    file::AbstractString...;
+    single=false,
+    gaps=false,
+    extended=false,
+    timebounds=false,
+    bysegment=false,
+    full=false,
+    sigdigs=nothing,
+    version=false,
+    help=false,
+    usage=false
+) 
+
+    args = String[]
+
+    single && push!(args, "-a")
+    gaps && push!(args, "-gaps")
+    extended && push!(args, "-ext")
+    timebounds && push!(args, "-tg")
+    bysegment && push!(args, "-tg")
+    full && push!(args, "-full")
+    
+    !isnothing(sigdigs) && push!(args, "-d $sigdigs")
+    version && push!(args, "-v")
+    help && push!(args, "-h")
+    usage && push!(args, "-u")
+
+    args = join(args, " ")
+    files = join(files, " ")
+    cmd = `$(CSPICE_jll.dskbrief()) $args $files`
+    @debug cmd
+    run(cmd)
+
+    nothing
+
+end
 
 
 """

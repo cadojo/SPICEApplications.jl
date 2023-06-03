06fb89a1d30369ad5458b1cbc2281b8cc18699d3
Joe Carpinelli <joseph.d.carpinelli@loopy.software>
2023-06-03 18:46:03 -0500

    Adding dskexp

--- src/SPICEApplications.jl
+++ src/SPICEApplications.jl
@@ -371,8 +371,38 @@ end
 
 """
 DSKEXP is a command-line program that exports data from DSK files to text files.
+
+# Extended Help
+
+!!! warning
+    All descriptions below were manually parsed from the commandline program's help/usage output.
+
+| Argument | Equivalent | Description | 
+| :--- | :--- | :--- |
+| `dsk` | `-dsk <dsk>` | DSK kernel |
+| `text` | `-text <output name>` | output name |
+| `format` `-format <MKDSK format code/name>` | MKSDK format code/name | 
+| `precision` | `-prec <# of vertex mantissa digits (1:17)` | number of vertex mantissa digits |
 """
-function dskexp() end
+function dskexp(
+    ; dsk=nothing, text=nothing, format=nothing, precision=nothing
+) 
+
+    args = String[]
+
+    !isnothing(dsk) && push!(args, "-dsk $dsk")
+    !isnothing(text) && push!(args, "-text $text")
+    !isnothing(format) && push!(args, "-format $format")
+    !isnothing(precision) && push!(args, "-prec $precision")
+
+    args = join(args, " ")
+    cmd = `$(CSPICE_jll.dskexp()) $args`
+    @debug cmd
+    run(cmd)
+
+    nothing
+
+end
 
 
 """

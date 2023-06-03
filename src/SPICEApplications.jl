da4b515bc3c6e7c8d21e359b765557a4a3d670cc
Joe Carpinelli <joseph.d.carpinelli@loopy.software>
2023-06-03 18:26:23 -0500

    Adding ckbrief

--- src/SPICEApplications.jl
+++ src/SPICEApplications.jl
@@ -194,14 +194,77 @@ end
 
 """
 CKBRIEF is a command-line utility program that displays a contents and time coverage summary for one or more binary CK files.
+
+
+# Extended Help
+
+!!! warning
+    All descriptions below were manually parsed from the commandline program's help/usage output.
+
+| Argument | Equivalent | Description | 
+| :--- | :--- | :--- |
+| `dump` | `-dump` | display interpolation intervals boundaries |
+| `boundaries` | `-nm` | display segment boundaries |
+| `relframes` | `-rel` | display relative-to frames |
+| `idframes` | `-n` | display frames associated with structure IDs | 
+| `tabular` | `-t` | display summary in a tabular format |
+| `single` | `-a` | treat all files as a single file |
+| `bycoverage` | `-g` | display summary grouped by coverage |
+| `utc` | `-utc` | display times in UTC calendar date format |
+| `utcdoy` | `-utcdoy` | display times in UTC day-of-year format |
+| `sclk` | `-sclk` | display times as SCLK strings |
+| `dpsclk` | `-dpsclk` | display times as SCLK ticks |
+| `id` | `[ID]` | display summmary for structure with [ID] | 
+| `summarize` | `-f` | summarize kernels listed in the `[list]` file |
+| `help` | `-h` | display help |
+| `version` | `-v` | display version |
 """
 function ckbrief(
     files::AbstractString...;
     dump=false,
     boundaries=false,
     relframes=false,
-    idframes=false)
+    idframes=false,
+    tabular=false,
+    single=false,
+    bycoverage=false,
+    utc=false,
+    utcdoy=false,
+    sclk=false,
+    dpsclk=false,
+    id=nothing,
+    summarize=nothing,
+    help=false,
+    version=false
+)
 
+    args = String[]
+
+    dump && push!(args, "-dump")
+    boundaries && push!(args, "-nm")
+    relframes && push!(args, "-rel")
+    idframes && push!(args, "-n")
+    tabular && push!(args, "-t")
+    single && push!(args, "-a")
+    bycoverage && push!(args, "-g")
+    utc && push!(args, "-utc")
+    utcdoy && push!(args, "-utcdoy")
+    sclk && push!(args, "-sclk")
+    dpsclk && push!(args , "-dpsclk")
+    
+    !isnothing(id) &&  push!(args, id)
+    !isnothing(summarize) && push!(args, "-f $summarize")
+
+    help && push!(args, "-h")
+    version && push!(args, "-v")
+
+    args = join(args, " ")
+    files = join(file, " ")
+    cmd = `$(CSPICE_jll.ckbrief()) $files $args`
+    @debug cmd
+    run(cmd)
+
+    nothing
 end
 
 
